ab6311f66855d6dddadaf535d28b77fb07487e6c
Vladimir, Vilen Hakobyan <52594864+vilenhakobyan4444@users.noreply.github.com>
2019-09-24 19:15:22 +0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -7,3 +7,23 @@ function simpleArraySum(ar) {
     }
     return count;
 }
+
+
+
+ //BirthdayCakeCandles function below.
+
+function birthdayCakeCandles(ar) {
+    var count=0;
+    var max = ar[0];
+    for(var i = 0; i<ar.length; i++){
+        if(ar[i]>max){
+            max = ar[i];
+        }
+    }
+    for(var j = 0; j<ar.length; j++){
+        if(ar[j]===max){
+            count++;
+        }
+    }
+    return count;
+}
